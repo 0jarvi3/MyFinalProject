580506a4f7ecfe2ffd8ed2ea34d2b39e1601bb1c
mustafa kurt <mstfkurt07@gmail.com>
2023-01-11 19:18:06 +0300

    ContextFile

---
+++ DataAccess/Concrete/CustomerDal.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq.Expressions;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace DataAccess.Concrete;
+
+public class CustomerDal : ICustomerDal
+{
+    public List<Customer> GetAll(Expression<Func<Customer, bool>>? filter)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Customer Get(Expression<Func<Customer, bool>> filter)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Add(Customer entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Customer entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(Customer entity)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ DataAccess/Concrete/ProductDal.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq.Expressions;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace DataAccess.Concrete;
+
+public class ProductDal : IProductDal
+{
+    public List<Product> GetAll(Expression<Func<Product, bool>>? filter)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Product Get(Expression<Func<Product, bool>> filter)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Add(Product entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Product entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(Product entity)
+    {
+        throw new NotImplementedException();
+    }
+}
